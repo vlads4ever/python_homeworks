0bb43c62665a6ad9771d3d700cb6d4721cad36b6
vlads_geekbrains <sevgrad@list.ru>
2023-02-17 16:40:16 +0300

    hmwrk_3 upgraded

--- HomeWork_1/hmwrk_3.py
+++ HomeWork_1/hmwrk_3.py
@@ -8,6 +8,9 @@
 123456 -> no """
 
 ticket_number = int(input('Введите шестизначный номер билета: '))
+
+# Первый вариант
+
 first_sum = 0
 second_sum = 0
 first_half = 0
@@ -21,6 +24,36 @@ if ticket_number >= 100000 and ticket_number <= 999999:
             first_half += 1
         else:
             second_sum += digit
+
+    if first_sum == second_sum:
+        print('yes')
+    else:
+        print('no')
+else:
+    print('Такого номера не может быть')
+
+############################################################
+
+# Второй вариант
+
+first_sum = 0
+second_sum = 0
+first_half = 0
+flag = True
+
+if ticket_number >= 100000 and ticket_number <= 999999:
+    while flag:        
+        if ticket_number >= 10:            
+            if first_half < 3:
+                first_sum += ticket_number % 10
+                ticket_number //= 10
+                first_half += 1
+            else:
+                second_sum += ticket_number % 10
+                ticket_number //= 10
+        else:
+            second_sum += ticket_number
+            flag = False
 
     if first_sum == second_sum:
         print('yes')
