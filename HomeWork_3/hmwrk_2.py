1c7674ca8760f463085b61968c4e24a5059b3f9d
vladimir_skrypnikov <sevgrad@list.ru>
2023-02-23 21:02:59 +0300

    Third homework

---
+++ HomeWork_3/hmwrk_2.py
@@ -0,0 +1,38 @@
+""" Задача 18: Требуется найти в массиве A[1..N] самый близкий по
+величине элемент к заданному числу X. Пользователь в первой строке
+вводит натуральное число N – количество элементов в массиве. В
+последующих строках записаны N целых чисел Ai. Последняя строка
+содержит число X.
+5
+1 2 3 4 5
+6
+-> 5 """
+
+n = int(input())
+
+array = list()
+for i in range(n):
+    array.append(i)
+    print(f'{array[i]} ', end = '')
+
+print()
+x = int(input())
+
+if x > array[0]:
+    diff = x - array[0]
+else:
+    diff = array[0] - x
+
+diff_index = 0
+
+for i in range(1, n):
+    if x > array[i]:
+        current_diff = x - array[i]
+    else:
+        current_diff = array[i] - x
+    
+    if current_diff < diff:
+        diff = current_diff
+        diff_index = i
+    
+print(array[diff_index])
